96cecb2319e1c19c3027a41556d165036689a4b9
Neelam Lakra <neelam@knoldus.in>
2018-01-18 17:36:27 +0530

    assignment updated

--- src/main/scala/edu/knoldus/package.scala
+++ src/main/scala/edu/knoldus/package.scala
@@ -2,26 +2,44 @@ package edu
 
 package object knoldus {
 
-  def length(givenList:List[Int]):Int ={
-    givenList.foldRight(0)((_,n)=>n + 1)
+  def length[A](givenList: List[A]): Int = {
+    givenList.foldRight(0)((_, n) => n + 1)
   }
 
-  def subSequenceChecker(originalList:List[Int],subList:List[Int]):Boolean ={
+  def subSequenceChecker[A](originalList: List[A], subList: List[A]): Boolean = {
+
     val actualList = originalList.toStream
     val shortList = subList.toStream
-    actualList.tails exists(_.startsWith(shortList))
-
+    actualList.tails exists (_.startsWith(shortList))
+/*
+    def innerSequence[A](originalList: List[A], subList: List[A], tempSubList: List[A], count: Int): Boolean = {
+      subList match {
+        case head :: tail => if (head == originalList.head) innerSequence(originalList.tail, tail, tempSubList, count + 1)
+        else innerSequence(originalList, tempSubList, tempSubList, count + 1)
+        case Nil => if (count == tempSubList.length) true else false
+      }
+    }
+    innerSequence(originalList, subList, subList, 0)
+    */
   }
 
-  def concateLists(firstList:List[Int],secondList:List[Int]):List[Int]={
+
+  def concateLists[A](firstList: List[A], secondList: List[A]): List[A] = {
     firstList match {
-      case head::tail =>concateLists(secondList :+head,tail)
-      case head ::Nil =>secondList :+head
-      case _ =>secondList
+      case topElement :: remainingList => concateLists(remainingList, secondList :+ topElement)
+      case _ => secondList
     }
   }
 
-  //def splitList[A](l:List[A],f:A=>Boolean):(List[A],List[B])={
+  def splitList[A](givenList: List[A], func: A => Boolean): (List[A], List[A]) = {
+    def splitter(givenList: List[A], validList: List[A], invalidList: List[A]): (List[A], List[A]) = {
+      givenList match {
+        case head :: tail => if (func(head)) splitter(tail, validList :+ head, invalidList) else splitter(tail, validList, invalidList :+ head)
+        case Nil => (validList, invalidList)
+      }
+    }
 
-  //}
+    splitter(givenList, List(), List())
+  }
+
 }
